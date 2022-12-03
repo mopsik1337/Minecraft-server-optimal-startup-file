e57d80a0ed5e1912a4e2436494c2e7124e0fcf78
Milan <76396495+JaerTheBest@users.noreply.github.com>
2022-12-03 18:07:03 +0600

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,2 @@
+
+java -Xms6G -Xmx12G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -jar server.jar nogui
